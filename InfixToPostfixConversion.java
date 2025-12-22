7dc33561ababf8e7b1cc0d2d1f6a5502b982719e
Your Name <you@example.com>
2025-12-22 09:31:57 +0545

    string convertToPostFix

--- InfixToPostfixConversion.java
+++ InfixToPostfixConversion.java
@@ -38,6 +38,55 @@ public classInfixToPostfixConversion{
             if (character.isWhitespace(token)){
                 continue;
             }
+
+            // CASE 1: The token is an operand (Letter or digit)
+            if (character.isLetterOrDigit(token)){
+                // if it is a number or a variable, just add it directly to the postfix result
+                postfixResult.append(token);
+            }
+
+            // CASE 2: The token is opening paranthesis'('
+            else if(token =='('){
+                // Push '(' on the stack.
+                operatorStack.push(token);
+            }
+
+            // CASE 3: The token is closing paranthesis ')'
+            else if (token == ')'){
+                // POP operators from the stack and append to the postfix result
+                // until the matching '(' is found.
+
+                while(!operatorStack.isEmpty() && operatorStack.peek() !='('){
+                    postfixResult.append(operatorStack.pop());
+                }
+
+                // if the stack is empty, discard or remove '('
+                if (!operatorStack.isEmpty()&& operatorStack.peek()=='('){
+                    operatorStack.pop();
+                }
+
+                // CASE 4: The token is an operator (+,-,*,/) --
+                else{
+                    // while the stack is not empty And the top operator has 
+                    // greater or equal precedence than the current token:
+
+                    while(!operatorStack.isEmpty()&&
+                    getPrecedence(operatorStack.peek())>= getPrecedence(token))
+                    {
+                        // Crucial check: Donot push the opening parantheis '('
+                        // it belongs to CASE 3
+
+                        if (operatorStack.peek()=='('){
+                            break;
+                        }
+                        // pop the higher precedence operator and push it to the result
+                        postfixResult.append(operatorStack);
+                    }
+
+                    // Now, push the current operator on to the stack 
+                    operatorStack.push(token);
+                } 
+            }
         }
     }
 }
