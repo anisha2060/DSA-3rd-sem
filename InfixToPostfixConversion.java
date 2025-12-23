9dcee928befb57b2a4a42cd1aef96b961ced3def
Your Name <you@example.com>
2025-12-23 10:34:54 +0545

    scanning

--- InfixToPostfixConversion.java
+++ InfixToPostfixConversion.java
@@ -1,4 +1,5 @@
-public classInfixToPostfixConversion{
+import java.util.Stack;
+public class InfixToPostfixConversion{
 
     /** 
     * Determines the precedecne of an operator
@@ -23,24 +24,24 @@ public classInfixToPostfixConversion{
                 return 0;    //Everything else  like paranthesis
         }
     }
-    public static string convertToPostfix(String infixExpression){
+    public static String convertToPostfix(String infixExpression){
         // 1. Create a StringBuilder to build the final postfix expression
         StringBuilder postfixResult = new StringBuilder();
 
         // 2. Create a stack to hold the operators and paranthesis
-        Stack<character> operatorStack=new Stack<>();
+        Stack<Character> operatorStack=new Stack<>();
 
         // 3. Loop through every character in the Infix expression
         for (int i = 0; i<infixExpression.length(); i++){
             char token = infixExpression.charAt(i);
 
             // Ignore any blank spaces
-            if (character.isWhitespace(token)){
+            if (Character.isWhitespace(token)){
                 continue;
             }
 
             // CASE 1: The token is an operand (Letter or digit)
-            if (character.isLetterOrDigit(token)){
+            if (Character.isLetterOrDigit(token)){
                 // if it is a number or a variable, just add it directly to the postfix result
                 postfixResult.append(token);
             }
@@ -87,7 +88,134 @@ public classInfixToPostfixConversion{
                     operatorStack.push(token);
                 } 
             }
+            // 4. After scanning the expression, POP all the remaining operators from the stack
+
+            while(!operatorStack.isEmpty()){
+                postfixResult.append(operatorStack.pop());
+            }
+            return postfixResult.toString();
         }
+    }
+    public static void main(String[] args ){
+        String infixExpression = "A+B*C";
+        String postfixResult=convertToPostfix(infixExpression);
+        System.out.println("Infix Expression" + infixExpression);
+        System.out.println("Postfix Expression" + postfixResult);
     }
 }
 
+
+
+// import java.util.Stack;
+// public class InfixToPostfixConversion{
+//     private static int getPrecedence(char op){
+//         switch (op){
+//             case'+':
+//             case'-':
+//              return 1;//Lowest precedence :Addition and Subtraction
+
+//             case'*':
+//             case'/':
+//              return 2;//Medium precedence :Multiplication and Division
+
+//             case'^':
+//             case'$':
+//              return 3;//Highest precedence :Exponentiation and Dollar sign
+//             default:
+//                 return 0;//Everthing else like paranthesis
+//         }
+//     }
+//     public static String convertToPostfix(String infixExpression){
+//         //1.Create a StringBuilder to build the final postfix expression
+//         StringBuilder postfixResult=new StringBuilder();
+
+//         //2.Create a Stack to hold operators and paranthesis
+//          Stack<Character> operatorStack =new  Stack<>();
+
+//         //3.Loop through every character inthe infix expression 
+//         for(int i=0 ;i<infixExpression.length();i++){
+//             char token=infixExpression.charAt(i);
+        
+
+//         //Ignore any blank spaces
+//         if(Character.isWhitespace(token)){
+//             continue;
+//         }
+
+//         //Case 1:The token is an operand (Letter or Digit)
+//         if(Character.isLetterOrDigit(token)){
+//             //If it is a number or a variable ,just add it directly to the postfix result
+//             postfixResult.append(token);
+//         }
+
+//         //Case 2:The token is opening paranthesis
+
+//         else if (token=='('){
+//             //Push '(' on to the Stack.
+//             operatorStack.push(token);
+//         }
+
+//         //Case 3:The token is closing paranthesis')
+//         else if(token == ')'){
+//             //pop operators from the Stack and append to the postfix result 
+//             // until the matching ')' is found
+
+//             while(!operatorStack.isEmpty() && operatorStack.peek()!='('){
+//                 postfixResult.append(operatorStack.pop());
+//             }
+//             //if the Stack is empty,discard/remove'('
+//             if(operatorStack.isEmpty() && operatorStack.peek()=='('){
+//             operatorStack.pop();
+//             }
+//         }
+
+//         //CASE 4:the token is an operator (+,-,*,/)
+//         else{
+//             //While the Stack is not empty AND the top operator has 
+//             //greater or equal precendence then the current token:
+
+//             while(!operatorStack.isEmpty()&&
+//             getPrecedence(operatorStack.peek())>=getPrecedence(token)){
+//                 //Crucial check: Don't push the opening paranthesis '('
+//                 //it belongs to case 3
+
+//                 if(operatorStack.peek() =='('){
+//                     break;
+//                 }
+
+//                 //pop the higher precendence operator and push it to the result
+
+//                 postfixResult.append(operatorStack);
+//             }
+
+//             //Now,push the current operator on to the Stack 
+//             operatorStack.push(token);
+//         }
+//     }
+
+
+
+
+//         //4.After scanning the expression ,POP all the remaining operators from the Stack
+
+//         while(!operatorStack.isEmpty()){
+//             postfixResult.append(operatorStack.pop());
+//         }
+//         return postfixResult.toString();
+        
+//     }
+
+//     public static void main(String[] args){
+//         String infixExpression="A+B*C";
+
+//         String postfixResult=convertToPostfix(infixExpression);
+//         System.out.println("Infix Expression"+ infixExpression);
+//         System.out.println("postfix Expression"+ postfixResult);
+//     }
+// }
+
+// /**
+//  * Determines the precedence of an operator
+//  * @param op The operator character
+//  * @return An integer representing the precedence  
+//  */
