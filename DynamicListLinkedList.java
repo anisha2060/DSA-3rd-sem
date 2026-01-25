83bfc28ac70fd1ca7c4db9c91d3e11b82d32e40c
Your Name <you@example.com>
2026-01-25 11:39:07 +0545

    DynamicLinkedList

---
+++ DynamicListLinkedList.java
@@ -0,0 +1,31 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next=null;
+
+    }
+    
+}
+public class DynamicListLinkedList{
+    public static void main(String[] args){
+        Node head = new Node(20);
+        Node second = new Node(40);
+        Node third = new Node(40);
+
+        head.next = second;
+        second.next = third;
+
+        Node temp = head;
+
+        System.out.println("Elements are:");
+
+        while (temp != null){
+            System.out.println(temp.data);
+            temp=temp.next;
+        }
+    }
+}
+    
